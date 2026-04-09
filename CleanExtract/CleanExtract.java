27c0fb8762dc67b75431a027a6ac66744b541c2d
guiarthur7 <arthur.guillaume@ynov.com>
2026-04-09 17:38:55 +0200

    helloworld

--- CleanExtract/CleanExtract.java
+++ CleanExtract/CleanExtract.java
@@ -2,19 +2,17 @@ public class CleanExtract {
     public static String extract(String s) {
         if (s == null) return "";
         String res = "";
-
         for (String bloc : s.split("\\|")) {
             int debut = bloc.indexOf('.');
             int fin = bloc.lastIndexOf('.');
 
-            if (debut != -1 && fin != -1 && debut < fin) {
+            if (debut != -1 && fin > debut) {
                 res += bloc.substring(debut + 1, fin).trim() + " ";
             }
-            else {
-                res += bloc.replace(".", "").trim() + " ";
-            }
         }
-
+        if (res.trim().isEmpty() && !s.contains(".")) {
+            return s.replace('|', ' ').replaceAll("\\s+", " ").trim();
+        }
         return res.replaceAll("\\s+", " ").trim();
     }
 }
