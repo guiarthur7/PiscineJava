d7ec774e28f228afe9ab3d55a99b6cc58d04ad85
guiarthur7 <arthur.guillaume@ynov.com>
2026-04-09 16:39:33 +0200

    helloworld

--- StringReplace/StringReplace.java
+++ StringReplace/StringReplace.java
@@ -1,8 +1,11 @@
 public class StringReplace {
-    public static String replace(String s, CharSequence target, CharSequence replacement) {
-        if (s == null) {
-            return null;
-        }
+    public static String replace(String s, String target, String replacement) {
+        if (s == null) return null;
+        return s.replace(target, replacement);
+    }
+
+    public static String replace(String s, char target, char replacement) {
+        if (s == null) return null;
         return s.replace(target, replacement);
     }
 }
