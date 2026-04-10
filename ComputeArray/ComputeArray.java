3e7805c259413c3eb350764ee8a5b612f14bb58e
guiarthur7 <arthur.guillaume@ynov.com>
2026-04-10 14:03:42 +0200

    helloworld

--- ComputeArray/ComputeArray.java
+++ ComputeArray/ComputeArray.java
@@ -1,12 +1,14 @@
 public class ComputeArray {
     public static int[] computeArray(int[] array) {
         if (array == null) return null;
+
         int[] res = new int[array.length];
+
         for (int i = 0; i < array.length; i++) {
-            if (array[i] % 3 == 0) {
+            if (Math.floorMod(array[i], 3) == 0) {
                 res[i] = array[i] * 5;
             }
-            else if ((array[i] - 1) % 3 == 0 || (array[i] - 1) % 3 == -2) {
+            else if (Math.floorMod(array[i], 3) == 1) {
                 res[i] = array[i] + 7;
             }
             else {
