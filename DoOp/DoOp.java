cf1df119884cf7c1f59ccd2890f1a0918552630e
guiarthur7 <arthur.guillaume@ynov.com>
2026-04-10 16:16:38 +0200

    helloworld

--- DoOp/DoOp.java
+++ DoOp/DoOp.java
@@ -1,6 +1,6 @@
 public class DoOp {
     public static String operate(String[] args) {
-        if (args.length < 3) return "Erreur : arguments insuffisants";
+        if (args.length < 3) return "Error";
 
         int n = Integer.parseInt(args[0]);
         int n2 = Integer.parseInt(args[2]);
@@ -12,15 +12,15 @@ public class DoOp {
             case "*":
                 return String.valueOf(n * n2);
             case "/":
-                if (n2 == 0) return "Division par zéro !";
+                if (n2 == 0) return "Error";
                 return String.valueOf(n / n2);
             case "-":
                 return String.valueOf(n - n2);
             case "%":
-                if (n2 == 0) return "Modulo par zéro !";
+                if (n2 == 0) return "Error";
                 return String.valueOf(n % n2);
             default:
-                return "Opérateur inconnu";
+                return "Error";
         }
     }
 }
