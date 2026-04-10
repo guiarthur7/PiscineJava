0f4aa62ecaa7456049be3867d5abfa39506007e5
guiarthur7 <arthur.guillaume@ynov.com>
2026-04-10 13:48:24 +0200

    helloworld

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -1,5 +1,8 @@
 public class Palindrome {
     public static boolean isPalindrome(String s) {
+        if (s == null) {
+            return false;
+        }
         String res = "";
         for (int i = s.length() -1; i >= 0; i--) {
             res += s.charAt(i);
