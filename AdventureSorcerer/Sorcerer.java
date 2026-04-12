ec755bb4dc73e8307ff7298d400e81d78a608b2d
guiarthur7 <arthur.guillaume@ynov.com>
2026-04-12 17:23:24 +0200

    helloworld

--- AdventureSorcerer/Sorcerer.java
+++ AdventureSorcerer/Sorcerer.java
@@ -1,6 +1,6 @@
 public class Sorcerer extends Character implements Healer {
 
-    private int healCapacity;
+    private final int healCapacity;
 
     public Sorcerer(String name, int maxHealth, int healCapacity) {
         super(name, maxHealth);
