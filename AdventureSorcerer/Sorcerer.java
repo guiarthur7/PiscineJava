0a01fa2148d63d39e157f382732f7c35c3e45ca6
guiarthur7 <arthur.guillaume@ynov.com>
2026-04-12 17:22:42 +0200

    helloworld

---
+++ AdventureSorcerer/Sorcerer.java
@@ -0,0 +1,34 @@
+public class Sorcerer extends Character implements Healer {
+
+    private int healCapacity;
+
+    public Sorcerer(String name, int maxHealth, int healCapacity) {
+        super(name, maxHealth);
+        this.healCapacity = healCapacity;
+    }
+
+    @Override
+    public int getHealCapacity() {
+        return this.healCapacity;
+    }
+
+    @Override
+    public void heal(Character target) {
+        int newHealth = target.getCurrentHealth() + this.getHealCapacity();
+
+        if (newHealth >= target.getMaxHealth()) {
+            target.setCurrentHealth(target.getMaxHealth());
+        } else {
+            target.setCurrentHealth(newHealth);
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (this.getCurrentHealth() <= 0) {
+            return this.getName() + " is a dead sorcerer. So bad, it could heal " + this.healCapacity + " HP.";
+        } else {
+            return this.getName() + " is a sorcerer with " + this.getCurrentHealth() + " HP. It can heal " + this.healCapacity + " HP.";
+        }
+    }
+}
