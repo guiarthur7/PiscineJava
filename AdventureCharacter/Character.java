8d5bf3f7c5db7a6934f72875f5d2a23c9e2e7333
guiarthur7 <arthur.guillaume@ynov.com>
2026-04-12 13:51:33 +0200

    helloworld

---
+++ AdventureCharacter/Character.java
@@ -0,0 +1,33 @@
+public class Character {
+
+    public int maxHealth;
+    public int currentHealth;
+    public final String name;
+
+    public Character(String name, int maxHealth) {
+        this.name = name;
+        this.maxHealth = maxHealth;
+        this.currentHealth = maxHealth;
+    }
+
+    @Override
+    public String toString() {
+        if (this.currentHealth > 0) {
+            return String.format("%s : %d/%d", this.name, this.currentHealth, this.maxHealth);
+        } else {
+            return String.format("%s : KO", this.name);
+        }
+    }
+
+    public void takeDamage(int damages) {
+        if (damages >= this.currentHealth) {
+            this.currentHealth = 0;
+        } else {
+            this.currentHealth -= damages;
+        }
+    }
+
+    public void attack(Character target) {
+        target.takeDamage(9);
+    }
+}
