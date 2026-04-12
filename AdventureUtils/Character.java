36c662f0355f3d612bac76b762b800dcd031d6f9
guiarthur7 <arthur.guillaume@ynov.com>
2026-04-12 15:59:33 +0200

    helloworld

---
+++ AdventureUtils/Character.java
@@ -0,0 +1,85 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Character {
+
+    private final int maxHealth;
+    private int currentHealth;
+    private final String name;
+    private static List<Character> allCharacters = new ArrayList<>();
+
+    public Character(String name, int maxHealth) {
+        this.name = name;
+        this.maxHealth = maxHealth;
+        this.currentHealth = maxHealth;
+        allCharacters.add(this);
+    }
+
+    public int getCurrentHealth() {
+        return this.currentHealth;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getMaxHealth() {
+        return this.maxHealth;
+    }
+
+    @Override
+    public String toString() {
+        if (this.currentHealth > 0) {
+            return String.format("%s : %d/%d", this.name, this.currentHealth, this.maxHealth);
+        } else {
+            return String.format("%s : KO", this.name);
+        }
+    }
+
+    public void takeDamage(int damages) {
+        if (damages >= this.currentHealth) {
+            this.currentHealth = 0;
+        } else {
+            this.currentHealth -= damages;
+        }
+    }
+
+    public void attack(Character target) {
+        if (target != null) {
+            target.takeDamage(9);
+        }
+    }
+
+    public static String printStatus() {
+        String separator = "------------------------------------------\n";
+        StringBuilder sb = new StringBuilder();
+        sb.append(separator);
+
+        if (allCharacters.isEmpty()) {
+            sb.append("Nobody's fighting right now !\n");
+        } else {
+            sb.append("Characters currently fighting : \n");
+            for (Character c : allCharacters) {
+                sb.append(" - ").append(c.toString()).append("\n");
+            }
+        }
+
+        sb.append(separator);
+        return sb.toString();
+    }
+
+    public static Character fight(Character c1, Character c2) {
+        while (c1.getCurrentHealth() > 0 && c2.getCurrentHealth() > 0) {
+            c1.attack(c2);
+            if (c2.getCurrentHealth() <= 0) {
+                return c1;
+            }
+
+            c2.attack(c1);
+            if (c1.getCurrentHealth() <= 0) {
+                return c2;
+            }
+        }
+        return (c1.getCurrentHealth() > 0) ? c1 : c2;
+    }
+}
