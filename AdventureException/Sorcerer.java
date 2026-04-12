3e69030f0d35a39f7dcba641626bffacd8c523d2
guiarthur7 <arthur.guillaume@ynov.com>
2026-04-13 01:46:57 +0200

    helloworld

---
+++ AdventureException/Sorcerer.java
@@ -0,0 +1,52 @@
+public class Sorcerer extends Character implements Healer {
+
+    private final int healCapacity;
+
+    public Sorcerer(String name, int maxHealth, int healCapacity, Weapon weapon) {
+        super(name, maxHealth, weapon);
+        this.healCapacity = healCapacity;
+    }
+
+    @Override
+    public int getHealCapacity() {
+        return this.healCapacity;
+    }
+
+    @Override
+    public void heal(Character target) {
+        int newHealth = target.getCurrentHealth() + this.getHealCapacity();
+
+        if (newHealth >= target.getMaxHealth()) {
+            target.setCurrentHealth(target.getMaxHealth());
+        } else {
+            target.setCurrentHealth(newHealth);
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (this.getCurrentHealth() <= 0) {
+            return this.getName() + " is a dead sorcerer. So bad, it could heal " + this.healCapacity + " HP.";
+        } else {
+            return this.getName() + " is a sorcerer with " + this.getCurrentHealth() + " HP. It can heal " + this.healCapacity + " HP.";
+        }
+    }
+
+    @Override
+    public void attack(Character character) {
+        this.heal(this);
+        int damage = (this.getWeapon() != null)
+                ? this.getWeapon().getDamage()
+                : 10;
+        character.takeDamage(damage);
+    }
+
+    @Override
+    public void takeDamage(int amount) {
+        if (amount >= this.getCurrentHealth()) {
+            this.setCurrentHealth(0);
+        } else {
+            this.setCurrentHealth(this.getCurrentHealth() - amount);
+        }
+    }
+}
