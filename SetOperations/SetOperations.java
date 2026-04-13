154ec62bdc94726829c2c2e4ff7d4a75335e9992
guiarthur7 <arthur.guillaume@ynov.com>
2026-04-13 14:48:06 +0200

    helloworld

---
+++ SetOperations/SetOperations.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetOperations {
+    public static Set<Integer> union(Set<Integer> set1, Set<Integer> set2) {
+        Set<Integer> res = new HashSet<>(set1);
+        res.addAll(set2);
+        return res;
+    }
+
+    public static Set<Integer> intersection(Set<Integer> set1, Set<Integer> set2) {
+        Set<Integer> res = new HashSet<>(set1);
+        res.retainAll(set2);
+        return res;
+    }
+}
