4471915bf67de972690771c05b625ce6a645e9ba
guiarthur7 <arthur.guillaume@ynov.com>
2026-04-13 02:24:58 +0200

    helloworld

--- ListSearchIndex/ListSearchIndex.java
+++ ListSearchIndex/ListSearchIndex.java
@@ -3,6 +3,9 @@ import java.util.ArrayList;
 
 public class ListSearchIndex {
     public static Integer findLastIndex(List<Integer> list, Integer value) {
+        if (list == null) {
+            return null;
+        }
         int index = 0;
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i).equals(value)) {
