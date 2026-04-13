53e8f4706b7b316b1b628d818721cae0ad40148e
guiarthur7 <arthur.guillaume@ynov.com>
2026-04-13 02:23:38 +0200

    helloworld

--- ListSearchIndex/ListSearchIndex.java
+++ ListSearchIndex/ListSearchIndex.java
@@ -28,6 +28,9 @@ public class ListSearchIndex {
     }
 
     public static List<Integer> findAllIndexes(List<Integer> list, Integer value) {
+        if (list == null) {
+            return null;
+        }
         ArrayList<Integer> indexes = new ArrayList<>();
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i).equals(value)) {
