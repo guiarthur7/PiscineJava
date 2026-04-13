f99015175ea7f0ff3d6dbd0b6e89cc4cacdbf3a9
guiarthur7 <arthur.guillaume@ynov.com>
2026-04-13 02:12:42 +0200

    helloworld

--- ListEquals/ListEquals.java
+++ ListEquals/ListEquals.java
@@ -2,13 +2,11 @@ import java.util.List;
 
 public class ListEquals {
     public static boolean areListsEqual(List<String> list1, List<String> list2) {
-        if (list1 == null || list2 == null) {
-            return false;
-        }
         if (list1 == null && list2 == null) {
             return true;
         }
-        if (list1.size() != list2.size()) {
+
+        if (list1 == null || list2 == null || list1.size() != list2.size()) {
             return false;
         }
 
