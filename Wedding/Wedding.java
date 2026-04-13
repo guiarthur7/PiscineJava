cbf5fa2e727aa88058001e344ff0b6924e1521db
guiarthur7 <arthur.guillaume@ynov.com>
2026-04-13 15:57:32 +0200

    helloworld

---
+++ Wedding/Wedding.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Wedding {
+
+    public static Map<String, String> createCouple(Set<String> first, Set<String> second) {
+        Map<String, String> couples = new HashMap<>();
+        List<String> firstList = new ArrayList<>(first);
+        List<String> secondList = new ArrayList<>(second);
+        Collections.shuffle(firstList);
+        Collections.shuffle(secondList);
+
+        int size = Math.min(firstList.size(), secondList.size());
+
+        for (int i = 0; i < size; i++) {
+            couples.put(firstList.get(i), secondList.get(i));
+        }
+        return couples;
+    }
+}
