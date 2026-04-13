5a86bb303f0f41e3502159ee03f0c7887ffd1b96
guiarthur7 <arthur.guillaume@ynov.com>
2026-04-13 02:22:52 +0200

    helloworld

--- ListSearchIndex/ListSearchIndex.java
+++ ListSearchIndex/ListSearchIndex.java
@@ -16,6 +16,9 @@ public class ListSearchIndex {
     }
 
     public static Integer findFirstIndex(List<Integer> list, Integer value) {
+        if (list == null) {
+            return null;
+        }
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i).equals(value)) {
                 return i;
